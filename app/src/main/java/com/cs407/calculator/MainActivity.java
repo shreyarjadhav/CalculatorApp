dd3db7f1641e04e3e3206d7c0ec52b66e60b7e2e
shreyarjadhav <srjadhav@wisc.edu>
2023-09-24 20:31:07 -0500

    Calculator App Functioning

--- app/src/main/java/com/cs407/calculator/MainActivity.java
+++ app/src/main/java/com/cs407/calculator/MainActivity.java
@@ -2,10 +2,61 @@ package com.cs407.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void clickAdd(View view){
+        EditText myTextField1 = (EditText) findViewById(R.id.myTextField1);
+        EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
+        double a = Double.parseDouble(myTextField1.getText().toString());
+        double b = Double.parseDouble(myTextField2.getText().toString());
+        double c = a+b;
+        goToActivity(""+c);
+    }
+
+    public void clickSubtract(View view){
+        EditText myTextField1 = (EditText) findViewById(R.id.myTextField1);
+        EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
+        double a = Double.parseDouble(myTextField1.getText().toString());
+        double b = Double.parseDouble(myTextField2.getText().toString());
+        double c = a-b;
+        goToActivity(""+c);
+    }
+
+    public void clickMultiply(View view){
+        EditText myTextField1 = (EditText) findViewById(R.id.myTextField1);
+        EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
+        double a = Double.parseDouble(myTextField1.getText().toString());
+        double b = Double.parseDouble(myTextField2.getText().toString());
+        double c = a*b;
+        goToActivity(""+c);
+    }
+
+    public void clickDivide(View view) {
+        EditText myTextField1 = (EditText) findViewById(R.id.myTextField1);
+        EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
+        double a = Double.parseDouble(myTextField1.getText().toString());
+        double b = Double.parseDouble(myTextField2.getText().toString());
+        double c = a / b;
+        if(Double.isInfinite(c)){
+            Toast.makeText(MainActivity.this, "Divide By Zero Error", Toast.LENGTH_LONG).show();
+        }
+        else {
+            goToActivity("" + c);
+        }
+    }
+
+    public void goToActivity(String s){
+        Intent intent=new Intent(this, Results.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
